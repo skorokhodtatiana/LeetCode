83e031b89b785e058576d2f7f15045975fd0da32
tskorohod <tskorohod@lmteam.by>
2025-04-04 10:25:17 +0300

    Added an array modification task

--- tasksArray/index.js
+++ tasksArray/index.js
@@ -5,4 +5,25 @@ const merge = function(nums1, m, nums2, n) {
 	nums1.splice(m,n,...nums2);
 	nums1.sort((a,b) => a - b);
 };
+//nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//nums1 = [1], m = 1, nums2 = [], n = 0
+//nums1 = [0], m = 0, nums2 = [1], n = 1
+
+// Дан массив целых чисел numsи целое число val, удалить все вхождения valв nums in-place . Порядок элементов может быть изменен. Затем вернуть количество элементов, в numsкоторых не равныval .
+// Рассмотрим число элементов, в numsкотором не равны valбыть k, чтобы быть принятым, вам необходимо сделать следующие вещи:
+// Измените массив nums так, чтобы первые k элементы numsсодержали элементы, которые не равны val. Остальные элементы numsне важны, как и размер nums.
+// Возвращаться k.
+
+const removeElement = function(nums, val) {
+	for(let i = nums.length -1; i >= 0; i--) {
+		if(nums[i] === val) {
+				nums.splice(i,1);
+			}
+	}
+	return nums.length;
+}
+
+//nums = [3,2,2,3], val = 3
+//nums = [0,1,2,2,3,0,4,2], val = 2
+
 
