1776b43eb068a63414e0295d77cdfbf5f4ed6f64
tskorohod <tskorohod@lmteam.by>
2025-04-16 21:08:23 +0300

    second option to sort arr where not more than 2 times every number

--- tasksArray/index.js
+++ tasksArray/index.js
@@ -61,12 +61,26 @@ const removeDuplicates2 = function(nums) {
 		}
 
 		if (count <= 2) {
+			nums[index] = nums[i];
 			index++
 		}
 	}
 
 	nums.length = index;
 }
+
+const removeDuplicates3 = (nums) => {
+	let k = 2;
+
+	for (let i = 2; i < nums.length; i++) {
+		if (nums[i] !== nums[k - 2]) {
+		nums[k++] = nums[i];
+		}
+	}
+
+	nums.length = k;
+	return nums.length;
+};
 //nums = [1,1,1,2,2,3]
 //nums = [0,0,1,1,1,1,2,3,3]
 
